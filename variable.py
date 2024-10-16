afea283e0029747b8ec831bc69c536d26b2b3e47
Vasenak <dobrorasa@gmail.com>
2024-10-16 13:55:00 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,6 @@
+Homework = 12
+Time = 1.5
+course = "Python"
+print(Time/Homework)
+Execution_time = 0.125
+print("Курс",":",course,',' ,"всего задач", ":",Homework,',',"затрачено часов",":", Time,',',"среднее время выполнения",":",Execution_time,'.')
